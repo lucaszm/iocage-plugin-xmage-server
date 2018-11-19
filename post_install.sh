7759f0f703ced486ca93f81277b4109987f61869
Jurgen Segaert <jsegaert@yahoo.com>
2018-11-18 22:11:15 -0700

    Tweak post_install script

--- post_install.sh
+++ post_install.sh
@@ -6,7 +6,7 @@ USER_NAME=xmage
 USER_UID=1200
 
 # Create user
-pw useradd -n $USER_NAME -u $USER_UID -G games -d /nonexistent -w random
+pw useradd -n $USER_NAME -u $USER_UID -G games -d /nonexistent -s /usr/sbin/nologin -w no
 
 # Download XMAGE
 mkdir -p $INSTALL_DIR
@@ -18,12 +18,18 @@ FETCH_FILE=`basename $FETCH_URL`
 
 fetch $FETCH_URL
 tar xf $FETCH_FILE mage-server/ 
+
+# File cleanup
 rm  $FETCH_FILE
 chown -R xmage:xmage .
 
 cd mage-server
 chmod +x startServer.sh
 sed -i '' 's/$//' startServer.sh
+
+for file in `ls config`; do
+  sed -i '' 's/$//' ./config/$file
+done
 
 # parse the java command
 JAVA_CMD=$(awk '/^java/{print}' ./startServer.sh)
